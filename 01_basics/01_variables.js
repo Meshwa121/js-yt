b2afbc15dfaa647d95dba7601254feb653c0c412
Meshwa <meshwa.mv9900@gmail.com>
2026-07-05 06:10:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId= 144553
+let accountEmail= "meshwwa@google.com"
+var accountPassword= "12345"
+accountCity= "Jaipur"
+console.log(accountId)
+/*
+Prefer not to use var 
+beacuse of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+let name="Meshwa"
+let age= 22
+let isLoggedIn= false
+
+//number => 2 to power 53
+//bigint
+//null=> standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof age)
+console.log(typeof undefined);
+console.log(typeof null)
